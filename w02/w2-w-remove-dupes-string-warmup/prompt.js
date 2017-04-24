7aa2788354f214d8dd6e3904c42415c704d4f47f
Meghan Prestemon <m.m.hares@gmail.com>
2017-04-24 09:26:53 -0700

    implemented functions to pass tests

--- w02/w2-w-remove-dupes-string-warmup/prompt.js
+++ w02/w2-w-remove-dupes-string-warmup/prompt.js
@@ -12,13 +12,32 @@
 
 
 function removeDupes(input){
+	var uniqueString = '';
 
+	for (var i = 0; i < input.length; i++) {
+		var currChar = input[i];
+		if (uniqueString.indexOf(currChar) === -1) {
+			uniqueString += currChar;
+		}
+	}
+
+	return uniqueString;
 }
 
 //CI for case insensitive
 function removeDupesCI(input){
+	var uniqueString = '';
 
+	for (var i = 0; i < input.length; i++) {
+		var currChar = input[i];
+		if (uniqueString.toUpperCase().indexOf(currChar.toUpperCase()) === -1) {
+			uniqueString += currChar;
+		}
+	}
+
+	return uniqueString;
 }
+
 
 module.exports = {removeDupes:removeDupes,
 									removeDupesCI:removeDupesCI,
